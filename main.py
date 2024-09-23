2e31b15d5123c72e8f307fbf91d47dbe15b0ddb2
27prxblms <malcondev.lds@hotmail.com>
2024-09-23 14:16:10 -0300

    nomofy

--- main.py
+++ main.py
@@ -15,15 +15,9 @@ class main:
         Banner() # Logo ASCII da ferramenta.
 
         self.command_list = [
-            'clear',
-            'echo',
-            'ping',
-            'exit',
-            'composer',
-            'npm',
-            'mkdir',
-            'clear temp files',
-            'ip pinger'
+            'exit', 'help', 'clear', 'ls',
+            'clear temp files', 'ip pinger',
+            'py', 'python'
             ]
 
         self.run()
@@ -34,64 +28,50 @@ class main:
                 current_dir = os.getcwd() # Diretorio atual.
                 print(f'\n {current_dir}')
                 cmd = input(f" {fg.RED}BlackHorizon~$ {fg.WHITE}")
-                full_cmd = cmd # comando sem a separação em args.
                 args = cmd.split()
 
                 if cmd in self.command_list:
                     
                     if cmd == 'clear':
                         os.system('cls')
-    
-                    elif len(args) == 2:
-                        cmd = args[0]
-                        arg = args[1]
 
-                        if cmd == 'py' or cmd == 'python':
-                            os.system(f'{cmd} {arg}')
+                    elif cmd == 'help':
+                        print(f'\n{self.command_list}')
+                    
+                    elif cmd == 'exit':
+                        sys.exit()
+                    
+                    elif cmd == 'ls':
+                        os.system('dir /b')
+
+                    elif cmd == 'clear temp files':
+                        print('')
+                        DeleteTempFiles()
+
+                    elif cmd == 'ip pinger':
+                        print(pinger_list)
 
                     else:
-                        os.system(full_cmd)
+                        os.system(cmd)
+
+                elif len(args) == 2:
+                    cmd = args[0]
+                    arg = args[1]
+
+                    if cmd == 'py' or cmd == 'python':
+                        os.system(f'{cmd} {arg}')
+
+                    elif cmd == 'cd':
+                        try:
+                            os.chdir(arg)
+                        except:
+                            DelayPrint("\n - you need to enter a directory.\n - example: cd DirectoryName.\n")
+
+                    elif cmd == 'pinger':
+                        IPPingers(arg)
 
                 else:
-                    DelayPrint(f"\n   '{full_cmd}' this command does not exist \n")
-
-                # if cmd == 'help':
-                #     print(self.command_list)
-                
-                # if cmd == 'exit':
-                #     sys.exit()
-
-                # if cmd == 'clear':
-                #     os.system('cls')
-                
-                # if cmd == 'ls':
-                #     os.system('dir /b')
-
-                # if cmd == 'clear temp files':
-                #     print('')
-                #     DeleteTempFiles()
-
-                # if cmd == 'ip pinger':
-                #     print(pinger_list)
-
-                # if len(args) == 2:
-                #     cmd = args[0]
-                #     arg = args[1]
-
-                #     if cmd == 'cd':
-                #         try:
-                #             os.chdir(arg)
-                #         except:
-                #             DelayPrint("\n - you need to enter a directory.\n - example: cd DirectoryName.\n")
-
-                #     elif cmd == 'pinger':
-                #         IPPingers(arg)
-
-                # # executa comandos completos.
-                # elif cmd in self.command_list:
-                #     os.system(full_cmd)
-                # else:
-                #     DelayPrint(f"\n   '{full_cmd}' this command does not exist \n")
+                    DelayPrint(f"\n   '{cmd}' this command does not exist \n")
 
             except KeyboardInterrupt: # Fecha a ferramenta quando Ctrl-C for precionado.
                 DelayPrint(f"\n\n  Ctrl-C was pressed, type {fg.RED}exit {fg.WHITE}to exit the tool.\n ")
